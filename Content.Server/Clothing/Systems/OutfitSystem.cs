space-wizards/space-station-14

use proxy methods in EntitySystems (#43083)
use proxy methods

Content.Server/Clothing/Systems/OutfitSystem.cs
@@ -26,7 +26,7 @@ public sealed class OutfitSystem : EntitySystem
 
     public bool SetOutfit(EntityUid target, string gear, Action<EntityUid, EntityUid>? onEquipped = null, bool unremovable = false)
     {
-        if (!EntityManager.TryGetComponent(target, out InventoryComponent? inventoryComponent))
+        if (!TryComp(target, out InventoryComponent? inventoryComponent))
             return false;
 
         if (!_prototypeManager.TryIndex<StartingGearPrototype>(gear, out var startingGear))
@@ -35,7 +35,7 @@ public sealed class OutfitSystem : EntitySystem
         HumanoidCharacterProfile? profile = null;
         ICommonSession? session = null;
         // Check if we are setting the outfit of a player to respect the preferences
-        if (EntityManager.TryGetComponent(target, out ActorComponent? actorComponent))
+        if (TryComp(target, out ActorComponent? actorComponent))
         {
             session = actorComponent.PlayerSession;
             var userId = actorComponent.PlayerSession.UserId;
@@ -52,12 +52,12 @@ public sealed class OutfitSystem : EntitySystem
                 if (gearStr == string.Empty)
                     continue;
 
-                var equipmentEntity = EntityManager.SpawnEntity(gearStr, EntityManager.GetComponent<TransformComponent>(target).Coordinates);
+                var equipmentEntity = Spawn(gearStr, Comp<TransformComponent>(target).Coordinates);
                 if (slot.Name == "id" &&
-                    EntityManager.TryGetComponent(equipmentEntity, out PdaComponent? pdaComponent) &&
-                    EntityManager.TryGetComponent<IdCardComponent>(pdaComponent.ContainedId, out var id))
+                    TryComp(equipmentEntity, out PdaComponent? pdaComponent) &&
+                    TryComp<IdCardComponent>(pdaComponent.ContainedId, out var id))
                 {
-                    id.FullName = EntityManager.GetComponent<MetaDataComponent>(target).EntityName;
+                    id.FullName = Comp<MetaDataComponent>(target).EntityName;
                 }
 
                 _invSystem.TryEquip(target, equipmentEntity, slot.Name, silent: true, force: true, inventory: inventoryComponent);
@@ -68,12 +68,12 @@ public sealed class OutfitSystem : EntitySystem
             }
         }
 
-        if (EntityManager.TryGetComponent(target, out HandsComponent? handsComponent))
+        if (TryComp(target, out HandsComponent? handsComponent))
         {
-            var coords = EntityManager.GetComponent<TransformComponent>(target).Coordinates;
+            var coords = Comp<TransformComponent>(target).Coordinates;
             foreach (var prototype in startingGear.Inhand)
             {
-                var inhandEntity = EntityManager.SpawnEntity(prototype, coords);
+                var inhandEntity = Spawn(prototype, coords);
                 _handSystem.TryPickup(target, inhandEntity, checkActionBlocker: false, handsComp: handsComponent);
             }
         }
@@ -90,7 +90,7 @@ public sealed class OutfitSystem : EntitySystem
                 break;
 
             // Don't require a player, so this works on Urists
-            profile ??= EntityManager.TryGetComponent<HumanoidProfileComponent>(target, out var comp)
+            profile ??= TryComp<HumanoidProfileComponent>(target, out var comp)
                 ? HumanoidCharacterProfile.DefaultWithSpecies(comp.Species, comp.Sex)
                 : new HumanoidCharacterProfile();
             // Try to get the user's existing loadout for the role

Spessmann/ephemeral-space

Content.Server/Clothing/Systems/OutfitSystem.cs
@@ -26,7 +26,7 @@ public sealed class OutfitSystem : EntitySystem
 
     public bool SetOutfit(EntityUid target, string gear, Action<EntityUid, EntityUid>? onEquipped = null, bool unremovable = false)
     {
-        if (!EntityManager.TryGetComponent(target, out InventoryComponent? inventoryComponent))
+        if (!TryComp(target, out InventoryComponent? inventoryComponent))
             return false;
 
         if (!_prototypeManager.TryIndex<StartingGearPrototype>(gear, out var startingGear))
@@ -35,7 +35,7 @@ public sealed class OutfitSystem : EntitySystem
         HumanoidCharacterProfile? profile = null;
         ICommonSession? session = null;
         // Check if we are setting the outfit of a player to respect the preferences
-        if (EntityManager.TryGetComponent(target, out ActorComponent? actorComponent))
+        if (TryComp(target, out ActorComponent? actorComponent))
         {
             session = actorComponent.PlayerSession;
             var userId = actorComponent.PlayerSession.UserId;
@@ -52,12 +52,12 @@ public sealed class OutfitSystem : EntitySystem
                 if (gearStr == string.Empty)
                     continue;
 
-                var equipmentEntity = EntityManager.SpawnEntity(gearStr, EntityManager.GetComponent<TransformComponent>(target).Coordinates);
+                var equipmentEntity = Spawn(gearStr, Comp<TransformComponent>(target).Coordinates);
                 if (slot.Name == "id" &&
-                    EntityManager.TryGetComponent(equipmentEntity, out PdaComponent? pdaComponent) &&
-                    EntityManager.TryGetComponent<IdCardComponent>(pdaComponent.ContainedId, out var id))
+                    TryComp(equipmentEntity, out PdaComponent? pdaComponent) &&
+                    TryComp<IdCardComponent>(pdaComponent.ContainedId, out var id))
                 {
-                    id.FullName = EntityManager.GetComponent<MetaDataComponent>(target).EntityName;
+                    id.FullName = Comp<MetaDataComponent>(target).EntityName;
                 }
 
                 _invSystem.TryEquip(target, equipmentEntity, slot.Name, silent: true, force: true, inventory: inventoryComponent);
@@ -68,12 +68,12 @@ public sealed class OutfitSystem : EntitySystem
             }
         }
 
-        if (EntityManager.TryGetComponent(target, out HandsComponent? handsComponent))
+        if (TryComp(target, out HandsComponent? handsComponent))
         {
-            var coords = EntityManager.GetComponent<TransformComponent>(target).Coordinates;
+            var coords = Comp<TransformComponent>(target).Coordinates;
             foreach (var prototype in startingGear.Inhand)
             {
-                var inhandEntity = EntityManager.SpawnEntity(prototype, coords);
+                var inhandEntity = Spawn(prototype, coords);
                 _handSystem.TryPickup(target, inhandEntity, checkActionBlocker: false, handsComp: handsComponent);
             }
         }
@@ -90,7 +90,7 @@ public sealed class OutfitSystem : EntitySystem
                 break;
 
             // Don't require a player, so this works on Urists
-            profile ??= EntityManager.TryGetComponent<HumanoidAppearanceComponent>(target, out var comp)
+            profile ??= TryComp<HumanoidAppearanceComponent>(target, out var comp)
                 ? HumanoidCharacterProfile.DefaultWithSpecies(comp.Species)
                 : new HumanoidCharacterProfile();
             // Try to get the user's existing loadout for the role