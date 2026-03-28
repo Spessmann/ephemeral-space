07b806f1ff796a9843a5846657e540baf641437c
Moony <moony@hellomouse.net>
2026-03-28 09:06:58 -0700

    User font selection (#1544)
    * User font selection
    
    This enables users to select custom fonts in the accessibility settings. It should be a huge boon to e.g. dyslexic people, allowing them to use whatever font is better for them. Fonts can be selected from the user's system fonts.
    
    This entails a few things:
    
    1. We now have a proper central location for fonts, in the form of IFontSelectionManager. No more hardcoding .ttf file paths. The default fonts are now specified through FontFamilyStack, replacing the temporary NotoFontFamilyStack.
    
    2. Stylesheets now need to be hot reloaded, as they are immutable. This entails changing the API for IStylesheetManager to use a subscription model, so controls using specific stylesheets automatically get updated.
    
    Overlays have been updated to use IFontSelectionManager directly, as they don't use style sheets. I did not update existing controls that hardcode .ttf paths, as those should be moved to sheetlets anyway.
    
    Requires the new (unfinished) engine support for accessing system fonts.
    
    * Pheem our fonts.
    
    ---------
    
    Co-authored-by: PJB3005 <pieterjan.briers+git@gmail.com>

---
+++ Content.Client/Stylesheets/Fonts/FontSelectionManager.DefaultFonts.cs
@@ -0,0 +1,31 @@
+﻿using Robust.Shared.Utility;
+
+namespace Content.Client.Stylesheets.Fonts;
+
+internal sealed partial class FontSelectionManager
+{
+    private static readonly FontFamilyStack DefaultFontMain = FontFamilyStack.New()
+        .AddKind(FontKind.Regular, new ResPath("/Fonts/_ES/Tomorrow/Tomorrow-Regular.ttf"))
+        .AddKind(FontKind.Regular, new ResPath("/Fonts/NotoSans/NotoSansSymbols-Regular.ttf"))
+        .AddKind(FontKind.Bold, new ResPath("/Fonts/_ES/Tomorrow/Tomorrow-Bold.ttf"))
+        .AddKind(FontKind.Bold, new ResPath("/Fonts/NotoSans/NotoSansSymbols-Bold.ttf"))
+        .AddKind(FontKind.Italic, new ResPath("/Fonts/_ES/Tomorrow/Tomorrow-Italic.ttf"))
+        .AddKind(FontKind.Italic, new ResPath("/Fonts/NotoSans/NotoSansSymbols-Regular.ttf"))
+        .AddKind(FontKind.BoldItalic, new ResPath("/Fonts/_ES/Tomorrow/Tomorrow-BoldItalic.ttf"))
+        .AddKind(FontKind.BoldItalic, new ResPath("/Fonts/NotoSans/NotoSansSymbols-Bold.ttf"))
+        .AddExtra(new ResPath("/Fonts/NotoSans/NotoSansSymbols2-Regular.ttf"))
+        .AddExtra(new ResPath("/Fonts/NotoEmoji.ttf"))
+        .Build();
+
+    private static readonly FontFamilyStack DefaultFontTitle = DefaultFontMain;
+
+    private static readonly FontFamilyStack DefaultFontMachineTitle = FontFamilyStack.New()
+        .AddKind(FontKind.Regular, new ResPath("/Fonts/_ES/Geo/Geo-Regular.ttf"))
+        .Build();
+
+    private static readonly FontFamilyStack DefaultFontMonospace = FontFamilyStack.New()
+        .AddKind(FontKind.Regular, new ResPath("/Fonts/RobotoMono/RobotoMono-Regular.ttf"))
+        .AddKind(FontKind.Bold, new ResPath("/Fonts/RobotoMono/RobotoMono-Bold.ttf"))
+        .AddKind(FontKind.Italic, new ResPath("/Fonts/RobotoMono/RobotoMono-Italic.ttf"))
+        .Build();
+}
